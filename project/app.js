f4ef2ae9270d3b09448ba8a883cc19858b96f440
Christian Goñi <chistiansen@gmail.com>
2016-10-05 18:57:41 +0200

    validation first level

--- project/app.js
+++ project/app.js
@@ -30,8 +30,11 @@ app.post("/users", function(req, res) {
         password: req.body.password,
         password_confirmation: req.body.password_confirmation
     });
-    console.log(password_confirmation);
-    user.save(function() {
+    console.log(user.password_confirmation);
+    user.save(function(err) {
+        if (err) {
+            console.log(String(err));
+        }
         res.send("Guardamos tus datos");
     });
 })
