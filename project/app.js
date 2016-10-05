172d3b651273a527f33895dea702c83497ec66b5
Christian Goñi <chistiansen@gmail.com>
2016-10-05 16:45:07 +0200

    connected to mongo

--- project/app.js
+++ project/app.js
@@ -6,13 +6,15 @@ var app = express();
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 // Conection to BBDD
-mongoose.connect("mongodb:localhost/myproject");
+mongoose.connect("mongodb://localhost/myproject");
 // BBDD's Schema
 var userSchemaJSON = {
     email: String,
     password: String
 };
 var user_schema = new Schema(userSchemaJSON);
+// modelo y schema a donde se mapea
+var User = mongoose.model("User", user_schema);
 
 // usar middleware para servir archivos estaticos
 // archivos dentro de carpeta assets y public son accesibles
@@ -30,13 +32,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
+    // en arg2 = doc = respuesta a consulta
+    User.find(function(err, doc) {
+        console.log(doc);
+    });
     res.render('login');
 });
 
 app.post("/users", function(req, res) {
+    /*
     console.log("contraseña = " + req.body.password);
-    console.log("email = " + req.body.email);
-    res.send("Datos recibidos");
+    console.log("email = " + req.body.email);*/
+    var user = new User({ email: req.body.email, password: req.body.password });
+    user.save(function() {
+        res.send("Guardamos tus datos");
+    });
+    //res.send("Datos recibidos");
 })
 
 app.listen(8080);
