0737bb629dc919d61d0502c26569e28edd855abf
Christian Goñi <chistiansen@gmail.com>
2016-10-05 20:25:31 +0200

    wtf

--- project/models/user.js
+++ project/models/user.js
@@ -3,12 +3,22 @@ var Schema = mongoose.Schema;
 
 mongoose.connect("mongodb://localhost/myproject");
 
+// validaciones se ejecutan en metodo save
+var posibles_valores = ["M", "F"];
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w{2,3})+$/, "Coloca email valido"];
 var user_schema = new Schema({
     name: String,
-    username: String,
+    username: { type: String, required: true, maxlength: [50, "Username maximo de 50 carac"] },
     password: String,
-    email: String,
-    birth: Date
+    //email: {type: String, required: true},
+    email: { type: String, required: "El correo es obligatorio", match: email_match },
+    birth: Date,
+    age: { type: Number, min: [18, "Edad minima de 18"], max: 100 },
+    // si sex != values =>message
+    sex: {
+        type: String,
+        enum: { values: posibles_valores, message: "Opcion no valida, F o M" }
+    }
 });
 
 user_schema.virtual("password_confirmation").get(function() {
