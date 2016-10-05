824e0c6de68acfc9d492b6fea5800a1c411059ba
Christian Goñi <chistiansen@gmail.com>
2016-10-05 17:25:50 +0200

    model&connection imported from model

--- project/app.js
+++ project/app.js
@@ -1,27 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var User = require("./models/user");
 
-// BBDD
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-// Conection to BBDD
-mongoose.connect("mongodb://localhost/myproject");
-// BBDD's Schema
-var userSchemaJSON = {
-    email: String,
-    password: String
-};
-var user_schema = new Schema(userSchemaJSON);
-// modelo y schema a donde se mapea
-var User = mongoose.model("User", user_schema);
-
-// usar middleware para servir archivos estaticos
-// archivos dentro de carpeta assets y public son accesibles
 app.use(express.static('public'));
 app.use(express.static('assets'));
 
-app.use(bodyParser.json()); // peticiones application/json
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -32,7 +17,6 @@ app.get('/', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-    // en arg2 = doc = respuesta a consulta
     User.find(function(err, doc) {
         console.log(doc);
     });
@@ -40,14 +24,10 @@ app.get('/login', function(req, res) {
 });
 
 app.post("/users", function(req, res) {
-    /*
-    console.log("contraseña = " + req.body.password);
-    console.log("email = " + req.body.email);*/
     var user = new User({ email: req.body.email, password: req.body.password });
     user.save(function() {
         res.send("Guardamos tus datos");
     });
-    //res.send("Datos recibidos");
 })
 
 app.listen(8080);
