b320dbd9d43df9b5798484de23348edd8d8a48f7
Christian Goñi <chistiansen@gmail.com>
2016-10-06 18:29:37 +0200

    trying to use routes-

--- project/middlewares/session.js
+++ project/middlewares/session.js
@@ -1,7 +1,9 @@
 module.exports = function(req, res, next) {
     if (req.session.user_id) {
         res.redirect("/login");
+        console.log("redirige puto");
     } else {
+        console.log("por que no");
         next();
     }
 }
