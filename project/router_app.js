dd17170a950b63ff0a7bc546435f9244919911ac
Christian Goñi <chistiansen@gmail.com>
2016-10-07 11:45:03 +0200

    added view to upload files

--- project/router_app.js
+++ project/router_app.js
@@ -8,4 +8,31 @@ router.get("/", function(req, res) {
     res.render("app/home");
 });
 
+/** REST */
+router.get("/img/new", function(req, res) {
+    res.render("app/img/new");
+});
+router.get("/img/:id/edit", function(req, res) {
+
+});
+// imagen concreta id
+router.route("/img/:id")
+    .get(function(req, res) {
+
+    })
+    .put(function(req, res) {
+
+    })
+    .dele te(function(req, res) {
+
+    });
+// coleccion imagenes
+router.route("/img/:id")
+    .get(function(req, res) {
+
+    })
+    .put(function(req, res) {
+
+    });
+
 module.exports = router;
